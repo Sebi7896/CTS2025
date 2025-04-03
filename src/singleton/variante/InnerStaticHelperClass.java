56150023eef28ac61b84b358b607b2c5fa935f37
Sebi7896 <felix_iacob@yahoo.com>
2025-04-03 21:19:26 +0300

    commit

--- src/singleton/variante/InnerStaticHelperClass.java
+++ src/singleton/variante/InnerStaticHelperClass.java
@@ -1,6 +1,28 @@
 package singleton.variante;
 
+import java.net.Inet4Address;
+
 public class InnerStaticHelperClass {
 
     private static InnerStaticHelperClass INSTANCE;
+
+    private InnerStaticHelperClass() {}
+
+    private static final class InnerHelper {
+        private static final InnerStaticHelperClass INSTANCE = new InnerStaticHelperClass();
+    }
+    public static InnerStaticHelperClass getINSTANCE() {
+        return InnerHelper.INSTANCE;
+    }
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        InnerStaticHelperClass innerStaticHelperClass = InnerStaticHelperClass.getINSTANCE();
+        InnerStaticHelperClass innerStaticHelperClass2 = InnerStaticHelperClass.getINSTANCE();
+        if(innerStaticHelperClass2 == innerStaticHelperClass) {
+            System.out.println("egale");
+        }
+    }
 }
